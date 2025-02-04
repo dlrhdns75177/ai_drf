7ea36616247071d808fd1c85708ac2456a4bb57d
DESKTOP-LR64C9E\USER <sararhdns7517@naver.com>
2025-02-04 14:31:21 +0900

    prodcuts 앱 생성

--- api_pjt/api_pjt/settings.py
+++ api_pjt/api_pjt/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     #third-party app
+    "django_extensions",
     "django_seed",
     "rest_framework",
     'rest_framework_simplejwt.token_blacklist',
     #local app
     "articles",
     "accounts",
+    "products",
 ]
 
 MIDDLEWARE = [
