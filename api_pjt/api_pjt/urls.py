cf8da1d81697a1df6b7225856f0a290cd8dc14ff
DESKTOP-LR64C9E\USER <sararhdns7517@naver.com>
2025-02-05 15:01:55 +0900

    cache에 저장-redis 사용 전

--- api_pjt/api_pjt/urls.py
+++ api_pjt/api_pjt/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/articles/",include("articles.urls")),
     path("api/v1/accounts/",include("accounts.urls")),
-    
+    path("api/v1/products/",include("products.urls")),
 ]
 urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
--- api_pjt/products/views.py
+++ api_pjt/products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.core.cache import cache #cache 가져오기 (cache에는 key-value 들어감)-> 이전에는 모두 하드디스크에 저장했었음
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+#Look Aside 캐싱 전략
+@api_view(['GET'])
+def product_list(request):
+    cache_key = "product_list" #먼저 서버(django)가 cache에서 데이터 가져오려고 시도해야함
+    if not cache.get(cache_key): #그래서 key 만들어주고 그 key값을 가져오도록 함(근데 그 cache에 데이터가 없으면 = cahce miss)
+        print("cache miss")
+        products = Product.objects.all()
+        serializers = ProductSerializer(products, many=True)
+        json_response = serializers.data #현재 json 형태(딕셔너리)로 되어 있으니까
+        cache.set(cache_key,json_response,5) #앞에가 key 뒤에 value (이제 cache에 값이 존재)
+    response_date = cache.get(cache_key) #근데 cache에 저장된 데이터가 있으면 get으로 가져오면 됨
+    return Response(response_date)
